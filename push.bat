28a33ce0a3e9b64b98228f0cbb6219c8aa03ead5
BugBubbles <823091936@qq.com>
2022-10-24 01:05:15 +0800

    'cbf'

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@
 @git add -A
 @git status
 choice /T 30 /C yn /N /D y /M "If you comfirm to push, please press key 'y', or you press any other key or waiting over 30 seconds will kill this push execuation."
-@if %errorlevel%==1 git commit -m 'cbf' && git push && echo 'Your change has been SUCCESSFULLY pushed to the reporsitory'
+@if %errorlevel%==1 git commit -m 'cbf' && git push && echo Your change has been SUCCESSFULLY pushed to the reporsitory
 
 @if %errorlevel%==2 pause
