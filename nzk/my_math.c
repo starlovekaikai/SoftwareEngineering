6dcd19e1a7be0d0ae69c87e62364cc17aeab8e2b
BugBubbles <823091936@qq.com>
2022-10-23 22:07:06 +0800

    cbf

--- nzk/my_math.c
+++ nzk/my_math.c
@@ -44,6 +44,25 @@ int line_equ(ui_point point, int len, vector uni_vec, int x_val)
  *  Output   : the y axe coordinate of input
  *             point
  */
+//只考虑为y值为正的情况，为负另一半取反就可以生成了
 int circle_equ(ui_point center, int radius, vector uni_vec, int x_val)
+{
+}
+/*
+ * This function uses a ui_point, a length value
+ * and a unit vector to form a polygon eqution and
+ * returns a quantified coordinate in axe "y".
+ *
+ * - point   : the beginning point in the ui coordinate（图形坐标下，单位是像素）
+ * - len     : the length of radius
+ * - uni_vec : a unit vector representing the
+ *             direction of circle.
+ * - x_val   : the x axe coordinate of input
+ *             point
+ *  Output   : the y axe coordinate of input
+ *             point
+ */
+//只考虑为y值为正的情况，为负另一半取反就可以生成了
+int polygon_equ(ui_point center, int radius, vector uni_vec, int x_val)
 {
 }
